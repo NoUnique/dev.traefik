003cb3f3b28baec4b95033cfdd82897426ed679f
NoUnique <kofmap@gmail.com>
2021-09-07 21:30:24 +0900

    Fix to get cert_key.tar from router without frontend access

--- prepare_requirements.sh
+++ prepare_requirements.sh
@@ -6,9 +6,10 @@ cd ${PATH_WORKSPACE}
 
 # Step1: download certification keys from router
 ADDRESS_ROUTER=router.asus.com
-DIR_SECRET=secret
+DIR_SECRET=secrets
 
-scp ${ADDRESS_ROUTER}:/tmp/cert_key.tar ./
+#scp ${ADDRESS_ROUTER}:/tmp/cert_key.tar ./
+wget --no-check-certificate https://router.asus.com:8443/cert_key.tar
 mkdir -p ${DIR_SECRET}
 tar -xvf cert_key.tar -C ${DIR_SECRET}
 rm -f cert_key.tar
@@ -16,5 +17,5 @@ rm -f cert_key.tar
 # Step2: make username & password for HTTP basic auth
 USERNAME=${USER}
 HASHED_PASSWORD=$(openssl passwd -apr1)
-echo "${USER}:${HASHED_PASSWORD}" > ./${DIR_SECRET}/auth
+echo "${USER}:${HASHED_PASSWORD}" > ./${DIR_SECRET}/basic-auth
 
